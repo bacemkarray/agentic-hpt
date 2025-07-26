e9daa325349a5defa54c8aeb486443844367c98a
bacemkarray <bkarray1@gmail.com>
2025-07-26 13:52:46 +0100

    duplicated states

---
+++ agent/utils/states.py
@@ -0,0 +1,12 @@
+class Parameters(TypedDict):
+    max_depth: int
+    learning_rate: float
+    tune_n_estimators: int
+    subsample: float
+
+class TuningState(TypedDict):
+    status: str
+    params: Parameters
+    score: float
+    workers_done: Annotated[List, operator.add]
+    iteration: int
